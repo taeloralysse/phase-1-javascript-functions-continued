922bcd10dd7c625282bf84e0b7cea0591fd73628
Taelor Lambert <taeloralysse@gmail.com>
2023-03-17 09:51:46 -0400

    functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(task = "go to the office") {
+    return (`This Monday, I will ${task}.`)
+}
+function wrapAdjective(adjective, message = `You are`){
+    return function (special){
+        return `${message} ${adjective}${special}${adjective}!`
+}}
